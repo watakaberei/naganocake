2075cc5ba1f69027d42f2a569a5d1eac992b0c30
watakaberei <126738738+watakaberei@users.noreply.github.com>
2023-04-09 08:56:48 +0000

    更新

--- db/migrate/20230409055427_create_items.rb
+++ db/migrate/20230409055427_create_items.rb
@@ -1,7 +1,12 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
-
+      t.string :image_id
+      t.string :name, null: false
+      t.string :introduction, null: false
+      t.integer :genre_id, null: false
+      t.integer :price, null: false
+      t.boolean :is_active, default: true, null: false
       t.timestamps
     end
   end
