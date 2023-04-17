0d73e54424fa4535801ec314da0860e8ad7226a6
watakaberei <126738738+watakaberei@users.noreply.github.com>
2023-04-17 01:33:48 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+  scope module: :public do
+    root to: 'homes#top'
+    get '/about' => 'homes#about'
+  end
 
 # 管理者用
 # URL /admin/sign_in ...
@@ -13,9 +17,13 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :admin do
+    root to: 'homes#top'
     resources :items
-    
+
     resources :genres
 
+    resources :customers
+
+    resources :orders
   end
 end
